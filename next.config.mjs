2179356e1dd54995fd30dd26c57b9e8dd8f324c8
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2025-04-05 01:50:56 +0530

    refactor: Remove assetPrefix and optimizeFonts from nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "/_next" : "",
-  optimizeFonts: true,
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse"],
   },
